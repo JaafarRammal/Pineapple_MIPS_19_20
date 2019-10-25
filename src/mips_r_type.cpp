5d156b42fd208b59c870f4d2a3be647c8e3d073c
Jaafar Rammal <jaafarrammal@dyn3147-87.wlan.ic.ac.uk>
2019-10-25 20:39:00 +0100

    division functions

--- src/mips_r_type.cpp
+++ src/mips_r_type.cpp
@@ -28,7 +28,6 @@ void add(MIPS& mips, uint32_t rs, uint32_t rt, uint32_t rd){
     mips.registers[rd] = r;
     mips.npc += 1;
   }
-
 }
 
 void addu(MIPS& mips, uint32_t rs, uint32_t rt, uint32_t rd){
@@ -41,8 +40,37 @@ void And(MIPS& mips,uint32_t rs,uint32_t rt, uint32_t rd){
   mips.npc += 1;
 }
 
-void div(MIPS& mips, uint32_t rs, uint32_t rt);
-void divu(MIPS& mips, uint32_t rs, uint32_t rt);
+void div(MIPS& mips, uint32_t rs, uint32_t rt){
+  // check if dividing by zero
+  if(mips.registers[rt] == 0){
+    // arithmetic error dividng by zero
+    // [ARITHMATIC EXCEPTION]
+  }else{
+    // load unsigned operands
+    uint32_t rs_unsigned = mips.registers[rs];
+    uint32_t rt_unsigned = mips.registers[rt];
+    // insert quotient in Lo and reminder in Hi
+    mips.hi = rs_unsigned % rt_unsigned;
+    mips.lo = rs_unsigned / rt_unsigned;
+    mips.npc += 1;
+  }
+}
+
+void divu(MIPS& mips, uint32_t rs, uint32_t rt){
+  // check if dividing by zero
+  if(mips.registers[rt] == 0){
+    // arithmetic error dividng by zero
+    // [ARITHMATIC EXCEPTION]
+  }else{
+    // load signed operands
+    int32_t rs_signed = mips.registers[rs];
+    int32_t rt_signed = mips.registers[rt];
+    // insert quotient in Lo and reminder in Hi
+    mips.hi = rs_signed % rt_signed;
+    mips.lo = rs_signed / rt_signed;
+    mips.npc += 1;
+  }
+}
 
 void jalr(MIPS& mips, uint32_t rs, uint32_t rd);
 void jr(MIPS& mips, uint32_t rt);
