1c26da87cd205002038edb2ba6ec58d0c71b899b
Jaafar Rammal <jaafarrammal@dyn3144-31.wlan.ic.ac.uk>
2019-10-29 15:08:03 +0000

    init mips

--- src/mips_init.cpp
+++ src/mips_init.cpp
@@ -1,2 +1,16 @@
 #include "mips_init.hpp"
 
+void init_mips(MIPS& mips){
+
+  mips.hi = 0;
+  mips.lo = 0;
+  mips.registers.resize(32, 0);
+
+  mips.memory.resize(MEMORY_SIZE, 0);
+
+  mips.pc = ADDR_INSTR_OFFSET;
+  mips.npc = mips.pc + 1;
+
+}
+
+void checkAddress(MIPS&);
