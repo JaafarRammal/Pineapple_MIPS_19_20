d4e3422b0ab7d8c6cb0b65577b2395f3fadc3e8d
Jaafar Rammal <jaafarrammal@dyn3154-219.wlan.ic.ac.uk>
2019-10-30 21:08:06 +0000

    It finally works!

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,25 @@ int main(int argc, char* argv[]){
 	MIPS mips;
 	init_mips(mips);
 	importBitFile(mips, argv[1]);
-
-	return 0;
-
+	
+	while(true){
+		bool executed = false;
+		checkAddress(mips);
+		std::cerr<<std::bitset<32>(mips.memory[mips.pc])<<std::endl;
+		std::cerr<<mips.pc<<std::endl;
+		r_type(mips, executed);
+		i_type(mips, executed);
+		j_type(mips, executed);
+		if(!executed){
+			throw (static_cast<int>(Exception::INSTRUCTION));
+		}	
+		std::cerr<<"Registers are: ";
+		for(int i=0; i<mips.registers.size(); i++){
+			std::cerr<<mips.registers[i]<<" ";
+		}
+		std::cerr<<std::endl;
+		mips.pc = mips.npc;
+		std::cerr<<std::endl;
+		std::cerr<<std::endl;
+	}
 }
--- src/mips_init.cpp
+++ src/mips_init.cpp
@@ -10,17 +10,19 @@ void init_mips(MIPS& mips){
   mips.memory.resize(MEMORY_SIZE, 0);
 
   mips.pc = ADDR_INSTR_OFFSET;
-  mips.npc = mips.pc + 1;
+  mips.npc = mips.pc;
 
 }
 
 void checkAddress(MIPS& mips){
   if(((mips.pc < ADDR_INSTR_OFFSET) || (mips.pc > (ADDR_INSTR_OFFSET + ADDR_INSTR_SIZE - 1))) && (mips.pc != ADDR_NULL_OFFSET)){
 		// pc outside instruction memory range. Memory exception
+    std::cerr<<"oups\n";
     throw (static_cast<int>(Exception::MEMORY));
 	}
 	if(mips.pc == ADDR_NULL_OFFSET){
     // reached end of program at address zero
+    std::cerr<<"r2?\n";
 		uint32_t output = mips.registers[2] & 0x000000FF;
 		std::exit(output);
 	}
@@ -31,7 +33,7 @@ void importBitFile(MIPS& mips, std::string filename){
   std::ifstream inputFile(filename, std::ios::binary | std::ios::ate);
 
 	if(!inputFile.is_open()){
-		std::cout << "E: Could not open file" << std::endl;
+		std::cerr << "E: Could not open file" << std::endl;
 		throw(static_cast<int>(Error::IO));
 	}
 
@@ -65,7 +67,7 @@ void importBitFile(MIPS& mips, std::string filename){
 		instruction<<=8;
 		instruction += uint8_t(buffer[i+3]);
     mips.memory[ADDR_INSTR_OFFSET + i/4] = instruction;
-		// std::cout<<std::bitset<32>(t)<<std::endl;
+		// std::cout<<std::bitset<32>(instruction)<<std::endl;
   }
 
   delete[] buffer;
