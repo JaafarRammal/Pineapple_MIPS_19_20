d45d39a40c575da1819457ab67569c6ad6cbbda8
Jaafar Rammal <jaafarrammal@dyn3148-160.wlan.ic.ac.uk>
2019-11-01 15:29:05 +0000

    Interactive output

--- test.sh
+++ test.sh
@@ -11,21 +11,30 @@ do
     perl -ne 'print pack("B32", $_)' < $InputTxt > $file
 done
 
+s=0
+f=0
+
 for InputBin in test_bench/bin/*.bin
 do 
     result=$( basename "$InputBin" | cut -f 1 -d '.')
-    echo "Currently testing $result:"
+    echo -n "Currently testing $result"
     ./bin/simulator $InputBin &> "./test_bench/debuggerOutput/$result.txt"
     simOut=$?
     expectedResult=$(<test_bench/expected/$result.txt)
 
     if [ "$simOut" == "$expectedResult" ]
     then 
+        let "s=s+1"
+        echo " (Successful)"
         echo "Successful test $result" >> "./test_bench/results.txt"
     else 
+        let "f=f+1"
+        echo " (Failed)"
         echo "Failed test $result. Expected $expectedResult but got $simOut" >> "./test_bench/results.txt"
     fi
 
     rm $InputBin
 
 done
+
+echo -e "\nA total of $s successes and $f failures"
